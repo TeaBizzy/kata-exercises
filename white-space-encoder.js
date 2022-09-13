d3a36ae6f75d3f838e7d597016d582e5e31c89ad
Stefan <satalbot@protonmail.com>
2022-09-13 21:00:41 +0000

    Encodes white spaces in strings to work as a URL

---
+++ white-space-encoder.js
@@ -0,0 +1,16 @@
+function urlEncode(text) {
+  let encodedString = ""
+  for(let i = 0; i < text.length; i++) {
+    if(text[i] === " ") {
+      encodedString += "%20";
+    } else {
+      encodedString += text[i];
+    }
+  }
+
+  return encodedString;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
